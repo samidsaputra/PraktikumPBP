66a1612acb89b5364d3b5209e8eeb020149d4807
samidsaputra <160684678+samidsaputra@users.noreply.github.com>
2024-09-10 09:46:37 +0700

    Add files via upload

---
+++ Praktikum 2/pwi1/welcome.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    
+    <body>
+        <?php
+        echo "<h2>Selamat datang di Praktikum PWI</h2><br/>";
+        echo "Hari ini praktikum : \"Sintaks Dasar PHP\"<br/>";
+        echo "Nama : \"nama praktikan\"<br/>";
+        echo "NIM : \"nim praktikan\"<br/>";
+        ?>
+    </body>
+</html>
